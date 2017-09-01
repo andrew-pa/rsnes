13b2844888f7a978561ea058d0844b6df9cfeef7
Andrew Palmer <andrew.pa@outlook.com>
2017-09-01 07:20:17 -0700

    add sdl2; basic scanline code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![feature(ord_max_min)]
 #[allow(unused_variables)]
 #[allow(dead_code)]
+extern crate sdl2;
 
 mod cpu;
 mod ppu;
@@ -15,22 +16,48 @@ use nes_memory::*;
 use std::env;
 use std::io::*;
 
+use sdl2::event::Event;
+
 fn main() {
+    let sdl_context = sdl2::init().expect("init SDL2");
+    let sdl_video = sdl_context.video().expect("init SDL2 video");
+    let window = sdl_video.window("rsnes", 800, 600).position_centered().opengl().build().expect("create window");
+
     let mut cart = cartridge::Cartridge::new_from_ines(
         env::args().nth(1).or_else(|| env::var("ROMPATH").ok()).expect("cart path")).expect("load cart");
     let mut ppu = PictureProcessor {};
     let mut cpu = cpu::CPU::new(NesMemory::new(&mut cart, &mut ppu));
-    /*cpu.memory.write(0xfffe, 0x00); //start CPU at 0x0000
-    cpu.memory.write(0xffff, 0x00);
-    cpu.memory.write(0x0000, 0xe8); // inx
-    cpu.memory.write(0x0001, 0x4c); // jmp 0x0000
-    cpu.memory.write(0x0002, 0x00);*/
-    let mut ln = String::new();
-    let mut count = 0;
-    while count < 30 {
-        let last_pc = cpu.pc;
-        cpu.step();
-        if cpu.pc == last_pc { count += 1; }
-        cpu.write_state();
+
+    let mut event_pump = sdl_context.event_pump().expect("obtain event pump");
+    'frameloop: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => { break 'frameloop },
+                _ => {}
+            }
+        }
+
+        for scanline in 0..240 {
+            let mut scancycles = 0;
+            while scancycles < 113 {
+                scancycles += cpu.step();
+            }
+        }
+        //entering v-blank
+        ppu.vblank();
+        for scanline in 0..3 {
+            let mut scancycles = 0;
+            while scancycles < 113 {
+                scancycles += cpu.step();
+            }
+        }
+        // v-blank
+        for scanline in 0..20 {
+            let mut scancycles = 0;
+            while scancycles < 113 {
+                scancycles += cpu.step();
+            }
+        }
+
     }
 }
--- src/ppu.rs
+++ src/ppu.rs
@@ -3,6 +3,7 @@ pub struct PictureProcessor {
 }
 
 impl PictureProcessor {
+    pub fn vblank() { }
     pub fn read(&self, addr: u16) -> u8 { println!("ppu read @ ${:x}", addr); 0 }
     pub fn write(&self, addr: u16, val: u8) { println!("ppu write ${:x} = {:x}", addr, val); }
 }
